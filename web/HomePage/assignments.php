84c5a469c56a35fb472231a5ad254800137e6a9b
Jon STuTZ <stu15012@byui.edu>
2019-04-29 20:25:32 -0600

    updated the php file

--- web/HomePage/assignments.php
+++ web/HomePage/assignments.php
@@ -1,11 +1,11 @@
 <?php
-$assignment_links = new array();
+$assignment_links = array();
 $assignment_links[] = "assignment1.html";
-$name = substr($assignment_links[0],0, -4)
+$name = substr($assignment_links[0],0, -4);
 
 print("<ul>");
 for($i = 0; $i < sizeof($assignment_links); $i++){
-	print("<li><a href='".$assignment_links[i]."'>$name</a></li>")
+	print("<li><a href='".$assignment_links[i]."'>$name</a></li>");
 }
 print("</ul>")
 
