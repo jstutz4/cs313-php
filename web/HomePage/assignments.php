dd3af58fc0a1b6fadfd5612a270e0c6ac119e8bf
Jon STuTZ <stu15012@byui.edu>
2019-04-29 20:33:54 -0600

    added css file for php file and updated php file

--- web/HomePage/assignments.php
+++ web/HomePage/assignments.php
@@ -1,12 +1,21 @@
 <?php
 $assignment_links = array();
 $assignment_links[] = "assignment1.html";
-$name = substr($assignment_links[0],0, -4);
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Home | Assignments</title>
+    <link rel="stylesheet" type="text/css" href="assign.css" />
+</head>
+<body>
+<h1>Welcome To The Assignments Page</h1>
 print("<ul>");
 for($i = 0; $i < sizeof($assignment_links); $i++){
-	print("<li><a href='".$assignment_links[i]."'>$name</a></li>");
+	print("<li><a href='".$assignment_links[i]."'>".substr($assignment_links[i],0, -5)."</a></li>");
 }
 print("</ul>")
-
+</body>
+<html>
 ?>
