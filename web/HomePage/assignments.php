0c9e550229218c669c2f6193691d4884b572c5b8
Jon STuTZ <stu15012@byui.edu>
2019-05-01 18:22:41 -0600

    update php file

--- web/HomePage/assignments.php
+++ web/HomePage/assignments.php
@@ -8,9 +8,14 @@ print('
     <meta charset="utf-8" />
     <title>Home | Assignments</title>
     <link rel="stylesheet" type="text/css" href="assign.css" />
+	<link rel="stylesheet" type="text/css" href="home.css" />
 </head>
 <body>
-<h1>Welcome To The Assignments Page</h1> ');
+<h1>Welcome To The Assignments Page</h1>
+<nav>
+        <a href="home.html">Home</a>
+        <a href="assignments.php">Assignments</a>
+</nav>');
 print("<ul>");
 for($i = 0; $i < sizeof($assignment_links); $i++){
 	$name = substr($assignment_links[$i],0, -5);
