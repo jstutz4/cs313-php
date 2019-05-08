560e31651adc033b398c9b541b50cf614559668c
Jon STuTZ <stu15012@byui.edu>
2019-05-07 20:37:22 -0600

    updated browse.php

--- web/onlineCatolog/browse.js
+++ web/onlineCatolog/browse.js
@@ -12,6 +12,20 @@ function table3(query3) {
     }
 }
 
+function table4(query4) {
+    if (query4.matches) {
+        var url = "browse.php?numRow=" + "4";
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("items").innerHTML = this.responseText;
+            }
+        }
+        httpRequest.open("GET", url, true);
+        httpRequest.send();
+    }
+}
+
 function table5(query5) {
     if (query5.matches) {
         var url = "browse.php?numRow=" + "5";
@@ -29,6 +43,9 @@ function table5(query5) {
 var query3 = window.matchMedia("(max-width: 765px)");
 table3(query3);
 query3.addListener(table3);
-var query5 = window.matchMedia("(min-width: 766px)");
+var query4 = window.matchMedia("(max-width: 765px) && (min-width:900)");
+table4(query4);
+query4.addListener(table3);
+var query5 = window.matchMedia("(min-width: 901px)");
 table5(query5);
 query5.addListener(table5);
