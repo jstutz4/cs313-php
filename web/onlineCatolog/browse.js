cbdaf495122fb106dd2a4b1c8efbfd56b1c36c8b
Jon STuTZ <stu15012@byui.edu>
2019-05-07 21:04:41 -0600

    updated browse.php

--- web/onlineCatolog/browse.js
+++ web/onlineCatolog/browse.js
@@ -1,3 +1,33 @@
+function table1(query1) {
+    console.log("five");
+    if (query1.matches) {
+        var url = "browse.php?numRow=" + "1";
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("items").innerHTML = this.responseText;
+            }
+        }
+        httpRequest.open("GET", url, true);
+        httpRequest.send();
+    }
+}
+
+function table2(query2) {
+    console.log("five");
+    if (query2.matches) {
+        var url = "browse.php?numRow=" + "2";
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("items").innerHTML = this.responseText;
+            }
+        }
+        httpRequest.open("GET", url, true);
+        httpRequest.send();
+    }
+}
+
 function table3(query3) {
     console.log("three");
     if (query3.matches) {
@@ -43,12 +73,22 @@ function table5(query5) {
     }
 }
 
-var query3 = window.matchMedia("(max-width: 765px)");
+var query1 = window.matchMedia("(max-width: 400px)");
+table1(query1);
+query1.addListener(table1);
+
+var query2 = window.matchMedia("(min-width: 401px) and (max-width: 600px)");
+table2(query2);
+query2.addListener(table2);
+
+var query3 = window.matchMedia("(min-width: 601px) and (max-width: 800px)");
 table3(query3);
 query3.addListener(table3);
-var query4 = window.matchMedia("(min-width: 770px) and (max-width: 900px)");
+
+var query4 = window.matchMedia("(min-width: 801px) and (max-width: 950px)");
 table4(query4);
 query4.addListener(table4);
-var query5 = window.matchMedia("(min-width: 901px)");
+
+var query5 = window.matchMedia("(min-width: 951px)");
 table5(query5);
 query5.addListener(table5);
