28072666881bb8410a93cf450fa10c28f0a99675
Jon STuTZ <stu15012@byui.edu>
2019-05-07 20:33:22 -0600

    updated browse.php

--- web/onlineCatolog/browse.js
+++ web/onlineCatolog/browse.js
@@ -26,9 +26,9 @@ function table5(query5) {
     }
 }
 
-var query3 = window.matchMedia("(max-width: 450px)");
+var query3 = window.matchMedia("(max-width: 750px)");
 table3(query3);
 query3.addListener(table3);
-var query5 = window.matchMedia("(min-width: 456px)");
+var query5 = window.matchMedia("(min-width: 751px)");
 table5(query5);
 query5.addListener(table5);
