0f7c8f016809e9f23095e1e2cf4f6f0ebf7a24a6
Jon STuTZ <stu15012@byui.edu>
2019-05-21 18:46:02 -0600

    test home page

---
+++ web/cryptoCurrency/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Currency Tracker</title>
+    <--!<link rel="stylesheet" type="text/css" href="home.css" />
+    <script src="ta1.js"></script> !-->
+</head>
+<body>
+<p>crypto Currency tracker</p>
+</body>
+</html>
