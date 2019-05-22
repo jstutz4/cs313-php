0b675cfb7e3bbc96043580719c95d9d6a3d1f06a
Jon STuTZ <stu15012@byui.edu>
2019-05-21 19:54:27 -0600

    update

--- web/cryptoCurrency/add_user.js
+++ web/cryptoCurrency/add_user.js
@@ -3,7 +3,7 @@ function adduser() {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            //document.getElementById("items").innerHTML = this.responseText;
+            document.getElementById("table").innerHTML = this.responseText;
             console.log(this.responseText);
         }
     }
