ca7fc917660481777838498124766145a142599e
Jon STuTZ <stu15012@byui.edu>
2019-05-21 18:53:15 -0600

    test home page

--- web/cryptoCurrency/home.php
+++ web/cryptoCurrency/home.php
@@ -1,13 +1,23 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8" />
     <title>Currency Tracker</title>
-    <--!<link rel="stylesheet" type="text/css" href="home.css" />
+    <!--<link rel="stylesheet" type="text/css" href="home.css" />
     <script src="ta1.js"></script> !-->
 </head>
 <body>
 <p>crypto Currency tracker</p>
+	<div id="table">
+		<form>
+		<input type="text" name="user_id">
+		<input type="button" name="add_user" value="New User">
+		</form>
+	</div>
 </body>
 </html>
