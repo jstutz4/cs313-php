592ec765ef671636f8bffae78fc45598cb8cf51d
Jon STuTZ <stu15012@byui.edu>
2019-05-21 19:36:38 -0600

    update

--- web/cryptoCurrency/home.php
+++ web/cryptoCurrency/home.php
@@ -8,8 +8,8 @@ session_start();
 <head>
     <meta charset="utf-8" />
     <title>Currency Tracker</title>
-    <!--<link rel="stylesheet" type="text/css" href="home.css" />
-    <script src="ta1.js"></script> !-->
+    <!--<link rel="stylesheet" type="text/css" href="home.css" /> !-->
+    <script src="add_user.js"></script> 
 </head>
 <body>
 <p>crypto Currency tracker</p>
