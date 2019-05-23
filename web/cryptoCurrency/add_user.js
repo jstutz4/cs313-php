92715d9c87690a5a464064f24e26df3203a2d0bf
Jon STuTZ <stu15012@byui.edu>
2019-05-22 21:24:55 -0600

    update

--- web/cryptoCurrency/add_user.js
+++ web/cryptoCurrency/add_user.js
@@ -1,6 +1,6 @@
 function adduser() {
     console.log(document.getElementById("uid").value);
-    var url = "generate_table.php?user=" + document.getElementById("uid").value;
+    var url = "generate_table.php?user=" + document.getElementById("uid").value + "&currency=*";
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
