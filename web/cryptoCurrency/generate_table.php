204183cf2d00ab33179b6b3066594ef3be95993d
Jon STuTZ <stu15012@byui.edu>
2019-05-22 19:32:22 -0600

    update

--- web/cryptoCurrency/generate_table.php
+++ web/cryptoCurrency/generate_table.php
@@ -2,7 +2,7 @@
 $SESSION["userName"] = $_POST["user"];
 $SESSION["userID"];
 $user_name = $_GET["user"];
-print($SESSION["user"]);
+
 try
 {
   $dbUrl = getenv('DATABASE_URL');
@@ -30,16 +30,15 @@ foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
 	if($user_row['user_name'] == $user_name){
 		$userID = $user_row['user_id'];
 		$table = $table . "<tr><td>" . $user_row['user_id'] . "</td> <td>" . $user_row['user_name']. "</td>";
-
+	}
 		foreach ($db->query('SELECT user_id, name, price, volume FROM currency') as $currency_row){
 			if($currency_row['user_id'] == $userID ){
 				$table = $table + "<td>". $currency_row['name']."</td><td>".$currency_row['price']."</td><td>". $currency_row['volume']."</td></tr>";
 			}
 		}
-	}
 }
 
-print($table . '</table>'. $user_name);
+print($table . '</table>');
 /*
 $stmt = $db->prepare('SELECT * FROM table WHERE name=:name');
 $stmt->bindValue(':name', $user_name, PDO::PARAM_STR);
