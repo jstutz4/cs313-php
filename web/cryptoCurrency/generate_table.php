317e2bbaf16130c0abe354dd769e5ec349043882
Jon STuTZ <stu15012@byui.edu>
2019-05-22 20:29:26 -0600

    update

--- web/cryptoCurrency/generate_table.php
+++ web/cryptoCurrency/generate_table.php
@@ -28,6 +28,7 @@ $userID;
 $table = "<table><th>currency</th><th>price</th><th>volume</th><th>Invest</th>";
 //$add_user = "<tr><td>" . $user_row['user_id'] . "</td> <td>" . $user_row['user_name']. "</td>";
 $addBTN = '<td><input type="button" value="Invest" name="invest"></td>';
+/*
 foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
 	
 	if($user_row['user_name'] == $user_name){
@@ -39,6 +40,13 @@ foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
 		}
 	} 
 }
+*/
+
+oreach ($db->query('SELECT user_id, name, price, volume FROM currency WHERE user_id=123') as $currency_row){
+			if($currency_row['user_id'] == $userID ){
+				$table = $table ."<tr><td>". $currency_row['name']."</td><td>".$currency_row['price']."</td><td>". $currency_row['volume']."</td>".$addBTN."</tr>";
+			}
+		}
 
 
 
