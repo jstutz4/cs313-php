7e422ec11d6fe540b07a21cb56d826bb611c6472
Jon STuTZ <stu15012@byui.edu>
2019-05-24 16:43:41 -0600

    update

--- web/cryptoCurrency/generate_table.php
+++ web/cryptoCurrency/generate_table.php
@@ -1,12 +1,26 @@
 <?php
 session_start();
 ?>
-<nav>
-<form class="form-inline active-cyan-3 active-cyan-4">
-  <i class="fas fa-search" aria-hidden="true"></i>
-  <input id="search" class="form-control form-control-sm ml-3 w-25" type="text" placeholder="Search Currency Name" aria-label="Search">
-  <input type="button" value="search" onclick="searchName()">
-</form>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.php">Search Currency <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="invest.php">Investments</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="button" onclick="searchName()">Filter</button>
+    </form>
+  </div>
 </nav>
 <?php
 $user_name = $_GET["user"];
