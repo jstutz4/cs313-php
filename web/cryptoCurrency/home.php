109240e1291d7cfe77a8611662d1d7fe488bbf72
Jon STuTZ <stu15012@byui.edu>
2019-05-23 20:03:36 -0600

    update

--- web/cryptoCurrency/home.php
+++ web/cryptoCurrency/home.php
@@ -8,11 +8,21 @@ session_start();
 <head>
     <meta charset="utf-8" />
     <title>Currency Tracker</title>
+	<!--bootstrap !-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="onlineCatolog.css">
+
     <link rel="stylesheet" type="text/css" href="home.css" />
     <script src="add_user.js"></script> 
 </head>
 <body>
 <h1>Crypto Currency tracker</h1>
+<nav>
+<form class="form-inline active-cyan-3 active-cyan-4">
+  <i class="fas fa-search" aria-hidden="true"></i>
+  <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search">
+</form>
+</nav>
 	<div id="table">
 		<form>
 		<h4>Enter Your User ID or Click New User</h4>
