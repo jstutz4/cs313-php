ece054ff4aba832944afd41f542df9dc18514314
Jon STuTZ <stu15012@byui.edu>
2019-05-24 16:28:00 -0600

    update

--- web/cryptoCurrency/invest.php
+++ web/cryptoCurrency/invest.php
@@ -17,12 +17,52 @@ session_start();
 </head>
 <body>
 <h1>Crypto Currency tracker</h1>
-
+<nav>
+<form class="form-inline active-cyan-3 active-cyan-4">
+  <i class="fas fa-search" aria-hidden="true"></i>
+  <input id="search" class="form-control form-control-sm ml-3 w-25" type="text" placeholder="Search Currency Name" aria-label="Search">
+  <input type="button" value="search">
+</form>
+</nav>
 <div id="table">
 	<table>
 		<th>Currency</td>
 		<th>Price</td>
-		<th>Amount</td>
+		<th>Amount Invested</td>
+<?php
+	try
+	{
+	  $dbUrl = getenv('DATABASE_URL');
+
+	  $dbOpts = parse_url($dbUrl);
+
+	  $dbHost = $dbOpts["host"];
+	  $dbPort = $dbOpts["port"];
+	  $dbUser = $dbOpts["user"];
+	  $dbPassword = $dbOpts["pass"];
+	  $dbName = ltrim($dbOpts["path"],'/');
+
+	  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+	  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $ex)
+	{
+	  echo 'Error!: ' . $ex->getMessage();
+	  die();
+	}
+	$table;
+	foreach ($db->query('SELECT user_id, name, price, amount FROM amount_invested') as $user_row){
+		if($user_row['user_id'] == $_SESSION["userID"]){
+			foreach ($db->query('SELECT user_id, name, price, volume FROM currency') as $currency_row){
+				if($currency_row['user_id'] == $_SESSION["userID"] && $currency_row['name'] == $currency){
+					$table = $table ."<tr><td>". $currency_row['name']."</td><td>".$currency_row['price']."</td><td>". $currency_row['volume']."</td>".$addBTN."</tr>";
+				}
+			}
+		} 
+	}
+
+?>
 	</table>
 </div>
 </body>
