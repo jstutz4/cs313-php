163d463809a4cd7fd15ac077885562753324a960
Jon STuTZ <stu15012@byui.edu>
2019-05-23 20:32:50 -0600

    update

--- web/cryptoCurrency/home.php
+++ web/cryptoCurrency/home.php
@@ -21,7 +21,7 @@ session_start();
 <form class="form-inline active-cyan-3 active-cyan-4">
   <i class="fas fa-search" aria-hidden="true"></i>
   <input id="search" class="form-control form-control-sm ml-3 w-25" type="text" placeholder="Search Currency Name" aria-label="Search">
-  <input type="button" value="search">
+  <input type="button" value="search" onclick="searchName()">
 </form>
 </nav>
 	<div id="table">
