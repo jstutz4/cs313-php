aa032eed2bc8e855102a5e143ad12cec792dd7c7
Jon STuTZ <stu15012@byui.edu>
2019-05-23 21:01:04 -0600

    update

--- web/cryptoCurrency/generate_table.php
+++ web/cryptoCurrency/generate_table.php
@@ -2,8 +2,10 @@
 $user_name = $_GET["user"];
 
 if(!isset($user_name)){
+$table = $_SESSION["userID"] . $_SESSION["user_name"] . "<br>";
 $_SESSION["userID"] = $_SESSION["userID"];
 $_SESSION["user_name"] = $_SESSION["user_name"];
+$table = $_SESSION["userID"] . $_SESSION["user_name"] . "<br>";
 }
 
 
