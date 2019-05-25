36251b5ea6583cf094c801476e3f8325292fcccf
Jon STuTZ <stu15012@byui.edu>
2019-05-24 18:34:31 -0600

    update

--- web/cryptoCurrency/generate_table.php
+++ web/cryptoCurrency/generate_table.php
@@ -3,7 +3,7 @@ session_start();
 ?>
 <nav class="navbar navbar-light bg-light">
   <form class="form-inline">
-	<input type="button" class="nav-link" onclick="<?php print("showCurrency(" . $_SESSION['user_name'] . ")" ) ?>" value="Search Currency">
+	<input type="button" class="nav-link" onclick="<?php print("showCurrency(" . $_SESSION['user_name'] . ")" ) ?>" value="View Currency">
     <input class="form-control mr-sm-2" id="search" type="search" placeholder="Currency Name (ie bitcoin)" aria-label="Search">
     <button class="btn btn-outline-success my-2 my-sm-0" type="button">Search Currency Name</button>
   </form>
