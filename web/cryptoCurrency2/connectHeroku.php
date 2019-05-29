b428daa2dbdb3b176a97022d4793a08cc06a1fb2
Jon STuTZ <stu15012@byui.edu>
2019-05-28 19:03:06 -0600

    try2

---
+++ web/cryptoCurrency2/connectHeroku.php
@@ -0,0 +1,28 @@
+<?php
+$user_name = $_GET["user"];
+
+if(isset($user_name)){
+	$_SESSION["user_name"] = $user_name;
+}
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
--- web/cryptoCurrency2/generate_table.php
+++ web/cryptoCurrency2/generate_table.php
@@ -13,33 +13,7 @@ session_start();
 	</div>
 </nav>
 <?php
-$user_name = $_GET["user"];
-
-if(isset($user_name)){
-	$_SESSION["user_name"] = $user_name;
-}
-
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+include 'connectHeroku.php';
 $_SESSION['user_name'];
 $_SESSION['userID'];
 $userID;
