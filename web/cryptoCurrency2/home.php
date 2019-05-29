6d9e3fb9924d2932b95c7efe95c82f97f29d343e
Jon STuTZ <stu15012@byui.edu>
2019-05-28 20:36:18 -0600

    try2

--- web/cryptoCurrency2/home.php
+++ web/cryptoCurrency2/home.php
@@ -20,13 +20,13 @@ session_start();
  <!-- <form class="form-inline"> !-->
 	<div id="menujs">
 		<form action="home.php" method="GET">
-			<input type="button" class="nav-linkjs" onclick="home()" value="View Currency">
 			<input type="button" class="nav-linkjs" onclick="invest()" value="View Investments">
-			</div>
-			<div id="searchjs">
-			<input type="search" class="form-control mr-sm-2" id="search"  name="currency" placeholder="Currency (ie bitcoin)" aria-label="Search" required>
-			<input type="submit" value="Filter Currency">
-			<input type="button" value="logout" onclick="logout()">
+			<input type="button" class="nav-linkjs" onclick="home()" value="View Currency">
+	</div>
+	<div id="searchjs">
+		<input type="search" class="form-control mr-sm-2" id="search"  name="currency" placeholder="Currency (ie bitcoin)" aria-label="Search" required>
+		<input type="submit" value="Filter Currency">
+		<input type="button" value="logout" onclick="logout()">
 		<form>
 	</div>
 </nav>
