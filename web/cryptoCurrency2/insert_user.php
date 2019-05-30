7f7ee9cfe82027e425ded8a590d0ee0dd4b1e995
Jon STuTZ <stu15012@byui.edu>
2019-05-29 21:45:36 -0600

    login

--- web/cryptoCurrency2/insert_user.php
+++ web/cryptoCurrency2/insert_user.php
@@ -3,18 +3,12 @@
 $userName = htmlspecialchars($_POST['user_id']);
 
 include 'connectHeroku.php';
-print("done with connect");
-?>
-<?php
+
 $stmt = $db->prepare('INSERT INTO users(user_name) VALUES(:user_name)');
-print("done with prepare");
-?>
-<?php
 $stmt->bindValue(':user_name', $userName);
-print("done with bindValue");
-?>
-<?php
 $stmt->execute();
-print("done with execute");
-header("Location: login.php?title=Now Login With You New User Name");
+
+print($stmt);
+
+#header("Location: login.php?title=Now Login With You New User Name");
 ?>
