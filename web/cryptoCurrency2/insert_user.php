b553495b6df26f46f0f1196795f9d92cb7036097
Jon STuTZ <stu15012@byui.edu>
2019-05-29 21:47:16 -0600

    login

--- web/cryptoCurrency2/insert_user.php
+++ web/cryptoCurrency2/insert_user.php
@@ -8,7 +8,7 @@ $stmt = $db->prepare('INSERT INTO users(user_name) VALUES(:user_name)');
 $stmt->bindValue(':user_name', $userName);
 $stmt->execute();
 
-print($stmt);
+print("all good");
 
 #header("Location: login.php?title=Now Login With You New User Name");
 ?>
