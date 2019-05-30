e73fd253085d1c3320018063a5415bed1ee01968
Jon STuTZ <stu15012@byui.edu>
2019-05-29 19:43:43 -0600

    login

--- web/cryptoCurrency2/login.js
+++ web/cryptoCurrency2/login.js
@@ -2,6 +2,20 @@ function autoLogin() {
     window.location.href = "home.php";
 }
 
+function adduser() {
+
+    var url = "addUser.php";
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("table").innerHTML = this.responseText;
+            console.log(this.responseText);
+        }
+    }
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
+}
+
 
 /*
 function autoLogin(refresh) {
