7bf4892b848d11f1a7c21cd1243f968ae4b97b60
Jon STuTZ <stu15012@byui.edu>
2019-05-30 22:07:06 -0600

    login

--- web/cryptoCurrency2/addInvestment.php
+++ web/cryptoCurrency2/addInvestment.php
@@ -4,24 +4,24 @@ $user_name = htmlspecialchars($_GET['hidden']);
 $name = htmlspecialchars($_GET['currency']);
 $price = htmlspecialchars($_GET['price']);
 $amount = htmlspecialchars($_GET['amount']);
-
+$user_id;
 include 'connectHeroku.php';
 
-/*foreach ($db->query('SELECT user_name FROM users') as $user_row){
+foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
 	if($user_row['user_name'] == $user_name){
-		$unique = false;
+		$user_id = $user_row['user_id'];
 	}
 }
-*/
-print($user_name. $name . $price . $amount);
+
 $stmt = $db->prepare('INSERT INTO amount_invested(user_id, name, price,amount) VALUES(:user_id, :name, :price, :amount)');
 
-$stmt->bindValue(':user_id', $_SESSION['userID']);
-#$stmt->bindValue(':name', $name);
-#$stmt->bindValue(':price', $price);
-#$stmt->bindValue(':amount', $amount);
+$stmt->bindValue(':user_id', $user_id);
+$stmt->bindValue(':name', $name);
+$stmt->bindValue(':price', $price);
+$stmt->bindValue(':amount', $amount);
 #$stmt->execute();
 
+print($user_name. $name . $price . $amount .$user_id);
 
 
 ?>
