ce93e4761709a5065ce8bf176e9ab36272dc2d2e
Jon STuTZ <stu15012@byui.edu>
2019-05-30 20:39:09 -0600

    login

--- web/cryptoCurrency2/dataBase.js
+++ web/cryptoCurrency2/dataBase.js
@@ -3,7 +3,7 @@ function investing(button) {
     var currency = button.name;
     var row = document.getElementsByClassName(currency);
     
-    var name = row[0].value;
+    var name = row[0];
     var price = row[1];
     var amount = row[4];
     console.log(row);
