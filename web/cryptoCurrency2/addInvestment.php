b5ffc5a218451c96e5ed40798b575aa949e09a3e
Jon STuTZ <stu15012@byui.edu>
2019-05-30 22:28:53 -0600

    login

--- web/cryptoCurrency2/addInvestment.php
+++ web/cryptoCurrency2/addInvestment.php
@@ -5,6 +5,9 @@ $name = htmlspecialchars($_GET['currency']);
 $price = htmlspecialchars($_GET['price']);
 $amount = htmlspecialchars($_GET['amount']);
 $user_id;
+$update = false;
+$start_amount = 0;
+$rowID;
 include 'connectHeroku.php';
 
 foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
@@ -13,15 +16,30 @@ foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
 	}
 }
 
-$stmt = $db->prepare('INSERT INTO amount_invested(user_id, name, price,amount) VALUES(:user_id, :name, :price, :amount)');
+foreach ($db->query('SELECT ID, user_id, name, amount FROM amount_invested') as $user_row){
+	if($user_row['user_id'] == $user_id && $user_row['name'] == $name){
+		$update = true
+		$rowID = $user_row['ID'];
+		$start_amount = $user_row['amount'] + $amount;
+	}
+}
+if($update){
+	$stmt = $db->prepare('UPDATE amount_invested' . 'SET amount = :total' . 'WHERE ID = :rowID ');
+	$stmt->bindValue(':total', $start_amount);
+	$stmt->bindValue(':rowID', $rowID);
+	$stmt->execute();
+}
+else{
 
-$stmt->bindValue(':user_id', $user_id);
-$stmt->bindValue(':name', $name);
-$stmt->bindValue(':price', $price);
-$stmt->bindValue(':amount', $amount);
-$stmt->execute();
+	$stmt = $db->prepare('INSERT INTO amount_invested(user_id, name, price,amount) VALUES(:user_id, :name, :price, :amount)');
 
-print($user_name. $name . $price . $amount .$user_id);
+	$stmt->bindValue(':user_id', $user_id);
+	$stmt->bindValue(':name', $name);
+	$stmt->bindValue(':price', $price);
+	$stmt->bindValue(':amount', $amount);
+	$stmt->execute();
+}
+print($user_name. $name . $price . $amount .$user_id . $start_amount);
 
 
 ?>
