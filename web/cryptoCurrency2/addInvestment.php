f054aa10b6ec742b08b73bbb80fb79fd19c4f5fa
Jon STuTZ <stu15012@byui.edu>
2019-05-30 21:47:42 -0600

    login

--- web/cryptoCurrency2/addInvestment.php
+++ web/cryptoCurrency2/addInvestment.php
@@ -6,15 +6,13 @@ $amount = htmlspecialchars($_GET['amount']);
 include 'connectHeroku.php';
 
 print($name. $_SESSION['userID'] . $price . $amount);
-?>
-<?php
 $stmt = $db->prepare('INSERT INTO amount_invested(user_id, name, price,amount) VALUES(:user_id, :name, :price, :amount)');
 
-$stmt->bindValue(':user_id', $_SESSION['userID']);
-$stmt->bindValue(':name', $name);
-$stmt->bindValue(':price', $price);
-$stmt->bindValue(':amount', $amount);
-$stmt->execute();
+#$stmt->bindValue(':user_id', $_SESSION['userID']);
+#$stmt->bindValue(':name', $name);
+#$stmt->bindValue(':price', $price);
+#$stmt->bindValue(':amount', $amount);
+#$stmt->execute();
 
 
 
