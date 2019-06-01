edc1820d026b0d73ceec1dd4ca15c385677987f6
Jon STuTZ <stu15012@byui.edu>
2019-06-01 11:58:52 -0600

    good working

--- web/cryptoCurrency2/generate_table.php
+++ web/cryptoCurrency2/generate_table.php
@@ -15,63 +15,49 @@ if(!isset($_SESSION['user_name'])){
 	header('Location: logout.php');
 }
 if($isUser){
-$_SESSION['user_name'];
-$_SESSION['userID'];
-$userID;
-$table = "<table><th>currency</th><th>price</th><th>volume</th><th>Invest</th><th>Invest Amount</th>";
-//$add_user = "<tr><td>" . $user_row['user_id'] . "</td> <td>" . $user_row['user_name']. "</td>";
-$addBTN = '<td><input type="button" name="' . $currency_row['name'] . '" value="invest" onclick="investing()"></td><td><input type="number"> </td>';
-$currency = $_GET['currency'];
+	$_SESSION['user_name'];
+	$_SESSION['userID'];
+	$userID;
+	$table = "<table><th>currency</th><th>price</th><th>volume</th><th>Invest</th><th>Invest Amount</th>";
+	//$add_user = "<tr><td>" . $user_row['user_id'] . "</td> <td>" . $user_row['user_name']. "</td>";
+	$addBTN = '<td><input type="button" name="' . $currency_row['name'] . '" value="invest" onclick="investing()"></td><td><input type="number"> </td>';
+	$currency = $_GET['currency'];
 
-/*
 
-$stmt = $db->prepare('SELECT user_id, name, price, volume FROM currency WHERE user_id=:id');
-$stmt->execute(array(':id' => 123));
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($rows as $currency_row){
-	if($currency_row['user_id'] == $userID ){
-		$table = $table ."<tr><td>". $currency_row['name']."</td><td>".$currency_row['price']."</td><td>". $currency_row['volume']."</td>".$addBTN."</tr>";
-	}
-}
-*/
-
-if (isset($currency) && $currency != "") {
-	//$table = $table . "heres0";
-	foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
-		if($user_row['user_name'] == $_SESSION["user_name"]){
-			//$userID = $user_row['user_id'];
-			//$table = $table . "heres1" . $_SESSION["userID"] . $_SESSION["user_name"];
-			foreach ($db->query('SELECT user_id, name, price, volume FROM currency') as $currency_row){
-				if($currency_row['user_id'] == $_SESSION["userID"] && $currency_row['name'] == $currency){
-					$table = $table .'<tr class="'. $currency_row['name'] . '"><form action="addInvestment.php" method="GET"><td>'. $currency_row['name']."</td><td>".$currency_row['price']."</td><td>". $currency_row['volume']."</td>". '<td><input type="submit" name="' . $currency_row['name'] . '" value="invest" onclick="investing(this)"></td><td><input type="number"> </td>' ."</form></tr>";
+	if (isset($currency) && $currency != "") {
+		//$table = $table . "heres0";
+		foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
+			if($user_row['user_name'] == $_SESSION["user_name"]){
+				//$userID = $user_row['user_id'];
+				//$table = $table . "heres1" . $_SESSION["userID"] . $_SESSION["user_name"];
+				foreach ($db->query('SELECT user_id, name, price, volume FROM currency') as $currency_row){
+					if($currency_row['user_id'] == $_SESSION["userID"] && $currency_row['name'] == $currency){
+						$table = $table .'<tr class="'. $currency_row['name'] . '"><form action="addInvestment.php" method="GET"><td>'. $currency_row['name']."</td><td>".$currency_row['price']."</td><td>". $currency_row['volume']."</td>". '<td><input type="submit" name="' . $currency_row['name'] . '" value="invest" onclick="investing(this)"></td><td><input type="number"> </td>' ."</form></tr>";
+					}
 				}
-			}
-		} 
-		//
+			} 
+		}
 	}
-}
-elseif(isset($_SESSION["user_name"]) || isset($_SESSION['userID'])){
-	foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
-		if($user_row['user_name'] == $_SESSION["user_name"] || $user_row['user_id'] == $_SESSION['userID']){
-			$userID = $user_row['user_id'];
-			$_SESSION["userID"] = $userID;
-			$_SESSION["user_name"] = $_SESSION["user_name"];
-			foreach ($db->query('SELECT user_id, name, price, volume FROM currency') as $currency_row){
-				if($currency_row['user_id'] == $userID){
-					$table = $table .'<tr><td class="'. $currency_row['name'] . '">'. $currency_row['name'].'</td><td class="'. $currency_row['name'] . '">'.$currency_row['price']."</td><td>". $currency_row['volume']."</td>". '<td><input type="button" name="' . $currency_row['name'] . '" value="invest" onclick="investing(this)"></td><td class="'. $currency_row['name'] . '"><input type="number"> </td>' ."</tr></form>";
+	elseif(isset($_SESSION["user_name"]) || isset($_SESSION['userID'])){
+		foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
+			if($user_row['user_name'] == $_SESSION["user_name"] || $user_row['user_id'] == $_SESSION['userID']){
+				$userID = $user_row['user_id'];
+				$_SESSION["userID"] = $userID;
+				$_SESSION["user_name"] = $_SESSION["user_name"];
+				foreach ($db->query('SELECT user_id, name, price, volume FROM currency') as $currency_row){
+					if($currency_row['user_id'] == $userID){
+						$table = $table .'<tr><td class="'. $currency_row['name'] . '">'. $currency_row['name'].'</td><td class="'. $currency_row['name'] . '">'.$currency_row['price']."</td><td>". $currency_row['volume']."</td>". '<td><input type="button" name="' . $currency_row['name'] . '" value="invest" onclick="investing(this)"></td><td class="'. $currency_row['name'] . '"><input type="number"> </td>' ."</tr></form>";
+					}
 				}
-			}
-		} 
+			} 
+		}
 	}
-}
 
 
 	print($table . '</table>' . '<input type="hidden" id="hidden" name="'. $_SESSION['user_name'] . '" value="' . $_SESSION['user_name'] . '" readonly>');
 }
 else{
 	header('Location: logout.php');
-
 }
 
 ?>
