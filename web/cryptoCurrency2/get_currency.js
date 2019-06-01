12cc0dbc30ac03d4d3001d21033c70e739ddf52f
Jon STuTZ <stu15012@byui.edu>
2019-05-31 19:24:02 -0600

    update

--- web/cryptoCurrency2/get_currency.js
+++ web/cryptoCurrency2/get_currency.js
@@ -5,7 +5,12 @@ function getCurrency() {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
+            var info = JSON.parse(this.responseText);
+            console.log(info);
+            console.log(info[data]);
+            console.log(info[data][LTC]);
+            console.log(info[data][LTC]);
+            console.log(info[data][LTC][quote][USD][price]);
         }
     }
     httpRequest.open("GET", url, true);
