06cf0f190625cdee48f78b6a93ed9d5cd6cb963a
Jon STuTZ <stu15012@byui.edu>
2019-05-31 19:44:06 -0600

    update

--- web/cryptoCurrency2/API_currency.php
+++ web/cryptoCurrency2/API_currency.php
@@ -24,6 +24,8 @@ curl_setopt_array($curl, array(
 ));
 
 $response = curl_exec($curl); // Send the request, save the response
-print_r(json_decode($response, true)); // print json decoded response
+print($response);
+$values = (json_decode($response, true));// print json decoded response
+print($values);
 curl_close($curl); // Close request
 ?>
