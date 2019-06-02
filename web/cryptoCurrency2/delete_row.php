9bd1f75425b718d9ce9abd66afe6b15207050bba
Jon STuTZ <stu15012@byui.edu>
2019-06-01 18:23:21 -0600

    error

--- web/cryptoCurrency2/delete_row.php
+++ web/cryptoCurrency2/delete_row.php
@@ -8,7 +8,8 @@ $table = htmlspecialchars($_GET['table']);
 $rowID = htmlspecialchars($_GET['rowID']);
 $query = 'DELETE FROM ' . $table . ' WHERE id = '. $rowID.;
 print($query);
-
+?>
+<?php
 include 'connectHeroku.php';
 
 $stmt = $db->prepare($query);
