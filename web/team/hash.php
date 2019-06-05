b869bad45e9494ed849e4f9be469e636eb161d32
Jon STuTZ <stu15012@byui.edu>
2019-06-05 16:46:36 -0600

    hash

--- web/team/hash.php
+++ web/team/hash.php
@@ -1,7 +1,7 @@
 <?php
 $user_name = htmlspecialchars($_GET['user_name']);
 $password = password_hash(($_GET['password']), PASSWORD_DEFAULT);
-$passwords =(($_GET['password']), PASSWORD_DEFAULT);
+$passwords =(($_GET['password']));
 
 
 print($user_name . $password);
