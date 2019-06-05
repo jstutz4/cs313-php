0624016e6da4b754343389310f22a6fc94227703
Jon STuTZ <stu15012@byui.edu>
2019-06-05 16:55:13 -0600

    done!

--- web/team/hash.php
+++ web/team/hash.php
@@ -9,7 +9,7 @@ print($user_name . $password);
 include 'connectHeroku.php';
 
 $stmt = $db->prepare('INSERT INTO person (user_name, password) VALUES(:userName, :password)');
-$stmt->bindValue(':user_name', $user_name);
+$stmt->bindValue(':userName', $user_name);
 $stmt->bindValue(':password', $passwords);
 $stmt->execute();
 
