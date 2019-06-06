2a37e1698344d96d4b0e9dfee6123eb6f7209394
Jon STuTZ <stu15012@byui.edu>
2019-06-05 21:44:58 -0600

    update currency

--- web/cryptoCurrency2/update_currency.php
+++ web/cryptoCurrency2/update_currency.php
@@ -9,7 +9,7 @@ include 'connectHeroku.php';
 
 foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
 	if($user_row['user_id'] == $_SESSION["userID"]){
-		foreach ($db->query('SELECT money_id, name FROM currency') as $currency_row){
+		foreach ($db->query('SELECT money_id, user_id name FROM currency') as $currency_row){
 			if($currency_row['user_id'] == $_SESSION['userID']){
 				$currency_names[] = $currency_row['name'];
 			}
