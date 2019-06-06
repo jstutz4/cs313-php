9c4b0a4a752c52d2ee4fcd67ebd245df621b2b6c
Jon STuTZ <stu15012@byui.edu>
2019-06-05 21:30:41 -0600

    update currency

--- web/cryptoCurrency2/update_currency.php
+++ web/cryptoCurrency2/update_currency.php
@@ -10,11 +10,11 @@ foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
 	if($user_row['user_id'] == $_SESSION["userID"]){
 		foreach ($db->query('SELECT money_id, name FROM currency') as $currency_row){
 			if($currency_row['user_id'] == $_SESSION['userID']{
-				$currency_names = $currency_names + $currency_row['name'] +',';
+				$currency_names[] = array($currency_row['name']);
 			}
 		}
 	} 
 }
 
-print($currency_names);
+print ($currency_names[0] ' and ' $currency_names[1]);
 ?>
