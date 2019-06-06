c53a6462f12b45a51d4b4aa02725b0e529d028f8
Jon STuTZ <stu15012@byui.edu>
2019-06-05 22:35:12 -0600

    update currency

--- web/cryptoCurrency2/update_currency.php
+++ web/cryptoCurrency2/update_currency.php
@@ -15,7 +15,7 @@ foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
 		}
 	} 
 }
-for($i = 0 $i < count($currency_names)){
+for($i = 0; $i < count($currency_names); $i++){
 print($currency_names[$i]);
 }
 ?>
