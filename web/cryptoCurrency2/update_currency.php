447e48d156436727a07a7aacf8e10eeacbe29afb
Jon STuTZ <stu15012@byui.edu>
2019-06-06 19:06:58 -0600

    test update currency table

--- web/cryptoCurrency2/update_currency.php
+++ web/cryptoCurrency2/update_currency.php
@@ -10,7 +10,7 @@ foreach ($db->query('SELECT user_id, user_name FROM users') as $user_row){
 	if($user_row['user_id'] == $_SESSION["userID"]){
 		foreach ($db->query('SELECT money_id, user_id, name FROM currency') as $currency_row){
 			if($currency_row['user_id'] == $_SESSION['userID']){
-				$currency_names[] = $currency_row['name'];
+				$currency_names[] = $currency_row['currency_id'];
 			}
 		}
 	} 
