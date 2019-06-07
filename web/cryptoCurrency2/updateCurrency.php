01c6af5cf5519b9b626dc1306eb14b0654b67747
Jon STuTZ <stu15012@byui.edu>
2019-06-06 20:13:05 -0600

    test update currency table

--- web/cryptoCurrency2/updateCurrency.php
+++ web/cryptoCurrency2/updateCurrency.php
@@ -36,7 +36,7 @@ for($i = 0; $i < count($currency_names); $i++){
 	print("starting loop <br>");
 	$price = $values["data"][$symbols[$i]]["quote"]["USD"]["price"];
 	$volume = $values["data"][$symbols[$i]]["quote"]["USD"]["volume_24h"];
-	print("looking " . $price . $volume . $currency_names[$i] . $_SESSION['userID'] "<br>");
+	print("looking " . $price . $volume . $currency_names[$i] . $_SESSION['userID'] . "<br>");
 
 	$stmt = $db->prepare('UPDATE currency SET price = :prices, volume = :volumes WHERE name = \':currencyID\' AND user_id = :userID');
 	$stmt->bindValue(':prices', $price);
