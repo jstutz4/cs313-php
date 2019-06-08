00f16a992158bf72c66c9c414d5046e18f26559b
Jon STuTZ <stu15012@byui.edu>
2019-06-08 15:52:05 -0600

    test update currency table

--- web/cryptoCurrency2/filterTable.php
+++ web/cryptoCurrency2/filterTable.php
@@ -6,7 +6,7 @@ session_start();
 
 $tableID = htmlspecialchars($_GET['table']);
 $currency = htmlspecialchars($_GET['name']);
-
+include 'connectHeroku.php';
 print("working" . $_SESSION['userID']. "<br>" . $table . $currency);
 if($tableID == 'currency'){
 print("working currency table <br>");
@@ -15,7 +15,11 @@ print("working currency table <br>");
 		if($currency_row['user_id'] == $_SESSION["userID"] && $currency_row['name'] == $currency){
 			$table = $table .'<tr><td class="'. $currency_row['name'] . '">'. $currency_row['name'].'</td><td class="'. $currency_row['name'] . '" name='. $currency_row['symbol'] .'">'.$currency_row['price']."</td><td>". $currency_row['volume']."</td><td>".$currency_row['change'] . '</td><td><input type="button" name="' . $currency_row['name'] . '" value="invest" onclick="investing(this)"></td><td class="'. $currency_row['name'] . '"><input type="number"> </td><td><input type="button" value="delete" onclick="deleteRow(\'currency\', ' . $currency_row['money_id'].')"></td></tr>';  
 		}
+		elseif ($currency == "") {
+			$table = $table .'<tr><td class="'. $currency_row['name'] . '">'. $currency_row['name'].'</td><td class="'. $currency_row['name'] . '" name='. $currency_row['symbol'] .'">'.$currency_row['price']."</td><td>". $currency_row['volume']."</td><td>".$currency_row['change'] . '</td><td><input type="button" name="' . $currency_row['name'] . '" value="invest" onclick="investing(this)"></td><td class="'. $currency_row['name'] . '"><input type="number"> </td><td><input type="button" value="delete" onclick="deleteRow(\'currency\', ' . $currency_row['money_id'].')"></td></tr>';  
+		}
 	}
+	print($table);
 }
 else{
 print("working invest table <br>");
@@ -25,8 +29,13 @@ $table = '<table class="investments"><th>Currency</th><th>Amount Invested ($)</t
 			if($user_row['name'] == $currency){
 				$table = $table ."<tr><td>". $user_row['name']."</td><td>".$user_row['amount']."</td><td>". $user_row['coin'].'</td><td>' . $user_row['prechange'] . '</td><td>'. $user_row['amount_change'].'</td><td><input type="button" value="delete" name="amount_invested" onclick="deleteRow(\'amount_invested\', ' . $user_row['invest_id'] .')"></td></tr>';
 			}
+			elseif($currency == "") {
+				$table = $table ."<tr><td>". $user_row['name']."</td><td>".$user_row['amount']."</td><td>". $user_row['coin'].'</td><td>' . $user_row['prechange'] . '</td><td>'. $user_row['amount_change'].'</td><td><input type="button" value="delete" name="amount_invested" onclick="deleteRow(\'amount_invested\', ' . $user_row['invest_id'] .')"></td></tr>';
+			}
+
 		} 
 	}
+	print($table);
 }
 
 ?>
